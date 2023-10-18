a7555c1c441e16c9a009cf67ab7e3e4aa4772dff
Darius Tyson <dariust@ksu.edu>
2023-10-18 16:11:59 -0500

    Implemented particle system and fireworks

---
+++ GameProject4/IParticleEmitter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace GameProject4
+{
+    public interface IParticleEmitter
+    {
+        public Vector2 Position { get; set; }
+
+        public Vector2 Velocity { get; set; }
+    }
+}
